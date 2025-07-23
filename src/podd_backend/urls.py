d0e1d0fa7f714c5db9a28cbf7a34c2ed91dcc808
Robert Huselius <robert@huseli.us>
2025-07-24 00:08:57 +0200

    Added robots.txt

--- src/podd_backend/urls.py
+++ src/podd_backend/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 
 urlpatterns: list = [
     path("", include("spodcat.urls")),
     path("admin/", include("spodcat.contrib.admin.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain; charset=utf-8")),
 ]
 urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
